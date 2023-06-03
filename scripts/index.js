bfc293ae13a44f94d32dbbb649cb8a25875be01d
Egor Baranov <intergalacticsheep00@gmail.com>
2023-06-03 23:29:48 +0300

    feature: added edit function

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,35 @@ const addButton = document.querySelector('.profile__add-button');
 const closeButton = document.querySelector('.popup__close-button');
 const saveButton = document.querySelector('.popup__save-button');
 const popup = document.querySelector('.popup');
+const popupForm = document.querySelector('.popup__form');
+
+const profileName = document.querySelector('.profile__name');
+const profileWork = document.querySelector('.profile__work');
+
+let nameInput = document.querySelector('.popup__name');
+let workInput = document.querySelector('.popup__work');
+
+function setPopupInputValue() {
+  nameInput.value = profileName.textContent;
+  workInput.value = profileWork.textContent;
+}
+
+function setProfileValue() {
+  profileName.textContent = nameInput.value;
+  profileWork.textContent = workInput.value;
+}
+
+function saveChanges(evt) {
+  evt.preventDefault();
+  setProfileValue();
+  editClose();
+}
+
+popupForm.addEventListener('submit', saveChanges);
 
 function editOpen() {
   popup.classList.add('popup__opened');
+  setPopupInputValue();
 }
 
 editButton.addEventListener('click', editOpen);
@@ -15,5 +41,3 @@ function editClose() {
 }
 
 closeButton.addEventListener('click', editClose);
-
-
