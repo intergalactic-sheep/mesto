aefa65b055433e9e1fe29d9b5c916b70f8796237
Egor Baranov <intergalacticsheep00@gmail.com>
2023-06-23 01:59:45 +0300

    feature: added initial render with js

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,49 @@ const popupForm = document.querySelector('.popup__form');
 const profileName = document.querySelector('.profile__name');
 const profileWork = document.querySelector('.profile__work');
 
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function (item) {
+  const elements = document.querySelector('.elements');
+  const card = document.querySelector('#card').content;
+
+  const cardElement = card.querySelector('.elements__item').cloneNode(true);
+
+  cardElement.querySelector('.elements__image').src = item.link;
+  cardElement.querySelector('.elements__image').alt = item.name;
+  cardElement.querySelector('.elements__text').textContent = item.name;
+
+  console.log(typeof cardElement.querySelector)
+
+  elements.append(cardElement);
+});
+
 let nameInput = document.querySelector('.popup__input_type_name');
 let workInput = document.querySelector('.popup__input_type_work');
 
