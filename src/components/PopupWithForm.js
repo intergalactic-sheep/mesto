8c9671bef8446e59b72a8879b9e04f1592f4d4af
Egor Baranov <intergalacticsheep00@gmail.com>
2023-08-16 13:25:17 +0300

    bugfix: review 8-1 bugs fixed 2.0

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,9 +2,7 @@ import { Popup } from './Popup.js';
 
 export class PopupWithForm extends Popup {
   constructor(popupSelector, handleFormSubmit) {
-  constructor(popupSelector, handleFormSubmit) {
     super(popupSelector);
-    this._handleFormSubmit = handleFormSubmit;
     this._handleFormSubmit = handleFormSubmit;
   }
 
@@ -24,20 +22,15 @@ export class PopupWithForm extends Popup {
   setEventListeners() {
     this._inputList = this.popup.querySelectorAll('.popup__input');
     this._form = this.popup.querySelector('.popup__form');
-
-    this._form.addEventListener('submit', (evt) => {
     this._form.addEventListener('submit', (evt) => {
       evt.preventDefault();
       this._handleFormSubmit(this._getInputValues());
-      this._handleFormSubmit(this._getInputValues());
     });
-
     super.setEventListeners();
   }
 
   close() {
     super.close();
-    this._form.reset();
     this._form.reset();
   }
 }
